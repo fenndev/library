89e33f71db8ce37ee449c4a6aae7d7b8be96bf79
fenndev <fenndev@protonmail.com>
2022-03-23 20:33:57 -0700

    Removed extraneous function on Book constructor

--- main.js
+++ main.js
@@ -5,17 +5,6 @@ function Book(title, author, pageNum, hasRead) {
     this.author = author;
     this.pageNum = pageNum;
     this.hasRead = hasRead;
-    this.info = function() {
-        if(hasRead == true || hasRead == false) {
-            if(hasRead)
-                return;
-            else
-                return;
-        }
-        else {
-            return "Invalid argument for parameter hasRead.";   
-        }
-    }   
 }
 
 function addBookToLibrary(bookToAdd) {
