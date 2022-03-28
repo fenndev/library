ac923eff4bbaf5d85bbfe9d6436026fbb18664d2
fenndev <fenndev@protonmail.com>
2022-03-28 01:49:56 -0700

    Finialized basic form flow

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const addButton = document.querySelector(".add-btn");
 const bookShelf = document.querySelector(".bookshelf");
 const modal = document.querySelector(".modal");
 const cancelButton = document.querySelector(".cancel-btn");
-const submitButton = document.querySelector(".submit-btn");
+const bookForm = document.querySelector(".form");
 
 /* Event Listeners */
 
@@ -16,15 +16,26 @@ cancelButton.addEventListener('click', () => {
     hideModal();
 })
 
-submitButton.addEventListener('click', () => {
-    let titleToAdd = document.querySelector("#input-title").textContent;
-    let authorToAdd = document.querySelector("#input-author").textContent;
-    let pageNumToAdd = document.querySelector("#input-author").textContent;
-    let readBoolToAdd = document.querySelector(".read").checked;
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let titleToAdd = bookForm.elements['input-title'].value;
+    let authorToAdd = bookForm.elements['input-author'].value;
+    let pageNumToAdd = bookForm.elements['input-author'].value;
+    let readBoolToAdd = bookForm.elements['read'].value;
     let newBook = new Book(titleToAdd, authorToAdd, pageNumToAdd, readBoolToAdd);
     addBookToLibrary(newBook);
-    console.log(bookCollection);    
+    console.log(bookCollection);
 })
+
+// submitButton.addEventListener('click', () => {
+//     let titleToAdd = document.querySelector("#input-title").textContent;
+//     let authorToAdd = document.querySelector("#input-author").textContent;
+//     let pageNumToAdd = document.querySelector("#input-author").textContent;
+//     let readBoolToAdd = document.querySelector(".read").checked;
+//     let newBook = new Book(titleToAdd, authorToAdd, pageNumToAdd, readBoolToAdd);
+//     addBookToLibrary(newBook);
+//     console.log(bookCollection);
+// })
 
 
 function hideModal() {
