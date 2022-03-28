ec15df966594d0d401db445787d73e7cd0a8e142
fenndev <fenndev@protonmail.com>
2022-03-28 00:51:44 -0700

    Added basic Book input form functionality

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const addButton = document.querySelector(".add-btn");
 const bookShelf = document.querySelector(".bookshelf");
 const modal = document.querySelector(".modal");
 const cancelButton = document.querySelector(".cancel-btn");
+const submitButton = document.querySelector(".submit-btn");
 
 /* Event Listeners */
 
@@ -14,6 +15,17 @@ addButton.addEventListener('click', () => {
 cancelButton.addEventListener('click', () => {
     hideModal();
 })
+
+submitButton.addEventListener('click', () => {
+    let titleToAdd = document.querySelector("#input-title").textContent;
+    let authorToAdd = document.querySelector("#input-author").textContent;
+    let pageNumToAdd = document.querySelector("#input-author").textContent;
+    let readBoolToAdd = document.querySelector(".read").checked;
+    let newBook = new Book(titleToAdd, authorToAdd, pageNumToAdd, readBoolToAdd);
+    addBookToLibrary(newBook);
+    console.log(bookCollection);
+})
+
 
 function hideModal() {
     modal.style.display = "none";
